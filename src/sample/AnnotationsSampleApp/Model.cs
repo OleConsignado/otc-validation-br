775f922ebe97312cb8399338c49148fd9a4070e9
José Lídio de Oliveira Júnior <jose.junior@oletecnologia.com.br>
2018-10-30 17:21:39 -0300

    Projeto inicial, validação de cpf

---
+++ src/sample/AnnotationsSampleApp/Model.cs
@@ -0,0 +1,13 @@
+﻿using Otc.Validation.Br;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnnotationsSampleApp
+{
+    public class Model
+    {
+        [CpfAttribute(ErrorKey = "400.001", ErrorMessage = "Cpf do Cliente inválido.")]
+        public string Cpf { get; set; }
+    }
+}
