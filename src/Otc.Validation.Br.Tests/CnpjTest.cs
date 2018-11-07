5585d7d13117a4e6b0089d3fb2fd386679a32890
lidiooliveira <lidiojr@gmail.com>
2018-11-07 16:29:20 -0200

    Adicionado validação de cnpj

---
+++ src/Otc.Validation.Br.Tests/CnpjTest.cs
@@ -0,0 +1,54 @@
+﻿using Otc.Validation.Br.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Otc.Validation.Br.Tests
+{
+    public class CnpjTest
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Invalidos(string cpnj)
+        {
+            Assert.False(CnpjValidation.IsValid(cpnj));
+        }
+
+        [Theory]
+        [MemberData(nameof(DataValidos))]
+        public void Validos(string cpnj)
+        {
+            Assert.True(CnpjValidation.IsValid(cpnj));
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] { null },
+                new object[] { "" },
+                new object[] { " " },
+                new object[] { "\t" },
+                new object[] { "00.000.000/0000-00" },
+                new object[] { "00.000.000/0000-0" },
+                new object[] { "00000000000000" },
+                new object[] { "11111111111111" },
+                new object[] { "22222222222222" },
+                new object[] { "33333333333333" },
+                new object[] { "44444444444444" },
+                new object[] { "55555555555555" },
+                new object[] { "66666666666666" },
+                new object[] { "77777777777777" },
+                new object[] { "88888888888888" },
+                new object[] { "99999999999999" },
+            };
+
+        public static IEnumerable<object[]> DataValidos =>
+            new List<object[]>
+            {
+                new object[] { "24173185000102" },
+                new object[] { "42822222000130" },
+                new object[] { "02636194000141" }
+            };
+    }
+}
--- src/Otc.Validation.Br/CnpjValidation.cs
+++ src/Otc.Validation.Br/CnpjValidation.cs
@@ -1,13 +1,23 @@
-﻿namespace Otc.Validation.Br.Library
+﻿using System;
+
+namespace Otc.Validation.Br.Library
 {
     public class CnpjValidation
     {
         public static bool IsValid(string value)
         {
-            if (string.IsNullOrWhiteSpace(value))
-                return false;
+            return IsValidCore(value);
+        }
 
-            string cnpj = value;
+        public static bool IsValid(int value)
+        {
+            return IsValidCore(Convert.ToString(value));
+        }
+
+        private static bool IsValidCore(string cnpj)
+        {
+            if (string.IsNullOrWhiteSpace(cnpj))
+                return false;
 
             int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
             int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
@@ -39,6 +49,17 @@
             else
                 resto = 11 - resto;
             digito = digito + resto.ToString();
+
+            string[] listaCnpjsInvalidos = { "00000000000000", "11111111111111", "22222222222222", "33333333333333",
+                                            "44444444444444", "55555555555555", "66666666666666", "77777777777777",
+                                            "88888888888888", "99999999999999" };
+
+            foreach (var cnpjVerificado in listaCnpjsInvalidos)
+            {
+                if (cnpj.Equals(cnpjVerificado))
+                    return false;
+            }
+
             return cnpj.EndsWith(digito);
         }
     }
