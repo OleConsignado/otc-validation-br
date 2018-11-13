ab1e2d841e114cf3d28ef94e6ca923cdfdd4fa11
José Lídio de Oliveira Júnior <jose.junior@oletecnologia.com.br>
2018-11-13 13:19:02 -0200

    Ajuste nome e removido validação de string/int

---
+++ src/Otc.ComponentModel.Annotations/CnpjAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Otc.ComponentModel.DataAnnotations;
+using Otc.Validation.Br.Library;
+using System;
+
+namespace Otc.Validations.Br.Annotations
+{
+    public class CnpjAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            bool result;
+            result = CnpjValidation.IsValid(value as string);
+            return result;
+        }
+    }
+}
