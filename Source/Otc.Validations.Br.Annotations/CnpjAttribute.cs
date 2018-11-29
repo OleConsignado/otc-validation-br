d2b7348ffb44e913a3174dee6602a198ef21949b
Marco Aurélio <maureliobrasil@gmail.com>
2018-11-29 13:19:02 -0200

    aplicando cnpj

--- Source/Otc.Validations.Br.Annotations/CnpjAttribute.cs
+++ Source/Otc.Validations.Br.Annotations/CnpjAttribute.cs
@@ -1,12 +1,18 @@
 ﻿using Otc.ComponentModel.DataAnnotations;
-using Otc.Validations.Br;
 
 namespace Otc.Validations.Br.Annotations
 {
     public class CnpjAttribute : ValidationAttribute
     {
+        public bool IsRequired { get; set; } = true;
+
         public override bool IsValid(object value)
         {
+            var cnpj = value?.ToString();
+
+            if (string.IsNullOrEmpty(cnpj) && !IsRequired)
+                return true;
+
             return CnpjValidation.IsValid(value?.ToString());
         }
     }
--- Source/SampleApp/Program.cs
+++ Source/SampleApp/Program.cs
@@ -15,9 +15,18 @@ namespace AnnotationsSampleApp
             var optionalIsValid = IsValid(new Model {Cpf = "410.850.372-40", CpfOptional = null });        
 
             Console.WriteLine(optionalIsValid);
+
+            var cnpjRequiredIsValid = IsValid(new CnpjModel { Cnpj = "00.000.000/0000-00" });
+
+            Console.WriteLine(cnpjRequiredIsValid);
+
+            var cnpjRequiredIsOptional = IsValid(new CnpjModel { Cnpj = "32.971.074/0001-61", CnpjOptional = null });
+
+            Console.WriteLine(cnpjRequiredIsOptional);
+
         }
 
-        static bool IsValid(Model model)
+        static bool IsValid<T>(T model)
         {
             var validationContext = new ValidationContext(model, serviceProvider: null, items: null);
             var validationResults = new List<ValidationResult>();
